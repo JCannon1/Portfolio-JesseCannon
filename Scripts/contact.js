ca829e60e3ce2512cae5190183b56c3ce9360b90
JCannon1 <jessecannon1@hotmail.com>
2017-02-14 17:39:48 -0500

    deleted web config

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -16,4 +16,4 @@ var FirstContactParagraph = document.getElementById("FirstContactParagraph");
 
 var myContent = "If you would like to contact me for further information of my work please feel free to reach me by leaving your Name, Number and a short message of what you would like to discuss."
 
-FirstContactParagraph.textContent = myContent;
+FirstContactParagraph.textContent = myContent; 
