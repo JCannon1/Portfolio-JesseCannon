9cd0ce7520fcc130318ae19cd4b293025bbd3658
JCannon1 <jessecannon1@hotmail.com>
2017-02-14 18:58:26 -0500

    edited text

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,6 @@
 // File name: app.js 
 // Author's name: Jesse Cannon 
-// Web site name: http://jessecannon-miniportfolio.azurewebsites.net/index.html 
+// Web site name: http://miniportfolio-contactpageupdate-jessecannon.azurewebsites.net/index.html
 // File description: main page JavaScript for my mini portfolio 
 
 (function () {
@@ -21,13 +21,13 @@ console.log(firstHeading.textContent);
 // connecting myContent to my index.html page 
 let FirstParagraph = document.getElementById("FirstParagraph");
 
-let myContent = "Hello, my name is Jesse Cannon and my goal of this portfolio site is to show you all of my web design skills I have acquired from my time at Georgian College. My Projects page will display all of the top pieces of work I have done so far for visuals. While the Contact page will ask you for your contact information. I hope my page is of great use to you. Jesse Cannon.";
+let myContent = 'Hello, my name is Jesse Cannon and my goal of this portfolio site is to show you all of my web design skills I have acquired from my time at Georgian College. My Projects page will display all of the top pieces of work I have done so far for visuals. While the Contact page will ask you for your contact information. I hope my page is of great use to you. Jesse Cannon.';
 
 FirstParagraph.textContent = myContent;
 
 // added a second paragraph section where my image went
 var SecondParagraph = document.getElementById("SecondParagraph");
-var myHTMLContent = "<h2>About Me</h2>";
+var myHTMLContent = '<h2>About Me</h2>';
 
 SecondParagraph.innerHTML = myHTMLContent;
 
