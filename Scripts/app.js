e5ac6c3afdb9a071803fd9b9e8d38c7a891ecdf9
JCannon1 <jessecannon1@hotmail.com>
2017-02-14 18:34:03 -0500

    changed content variable names

--- Scripts/app.js
+++ Scripts/app.js
@@ -49,9 +49,9 @@ console.log(firstHeadingContact.textContent);
   // connecting myContent to my contact.html page 
 var FirstContactParagraph = document.getElementById("FirstContactParagraph");
 
-var myContent = "If you would like to contact me for further information of my work please feel free to reach me by leaving your Name, Number and a short message of what you would like to discuss."
+var mySecondContent = "If you would like to contact me for further information of my work please feel free to reach me by leaving your Name, Number and a short message of what you would like to discuss."
 
-FirstContactParagraph.textContent = myContent; 
+FirstContactParagraph.textContent = mySecondContent; 
     let FullName = document.getElementById("FullName");
     let ContactNumber = document.getElementById("ContactNumber");
     let Email = document.getElementById("Email");
