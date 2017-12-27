9ee66e8e0a80b00b06af2e7311ecce94623edba5
JCannon1 <jessecannon1@hotmail.com>
2017-12-27 14:20:44 -0500

    Second Commit

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -13,16 +13,21 @@ console.log(firstHeadingProjects.textContent);
 
 // created variable for "Project 1" text content 
 var FirstProject = document.getElementById("FirstProject");
-var myContent = "This first project is one of my favourites as it has a unique design to it and displays my ability to make lists. I also like the use of the background image to add to the overall theme.";
+var myContent = "This first project is one of my favourites as it has a unique design to it and displays my ability to grab data from a database and display it in a table. This project also demonstrates my ability to create working register and login applications as well as adding in authorization and authentication to my pages. I also like the use of the background image to add to the overall theme.";
 FirstProject.textContent = myContent;
 
 // created variable for "Project 2" text content 
 var SecondProject = document.getElementById("SecondProject");
-var myHTMLContent = "My second project also shows my ability with HTML, CSS and navigation lists. However, this project was solely designed and created by me and each page has a different style to it. A long with doing this, I was also able to keep the same theme going on as well.";
+var myHTMLContent = "My second project shows my design skills with many features such as Bootstrap carousel, accordian and dropdowns.There are many interactive features on this page that were made all form HTML, CSS and JavaScript.";
 SecondProject.innerHTML = myHTMLContent;
 
 // created variable for "Project 3" text content 
 var ThirdProject = document.getElementById("ThirdProject");
-var myThirdContent = "This final project is my favourite piece because I feel I was successfully able to implement several unique design structures into it and it turned out just the way I wanted it to.";
+var myThirdContent = "This next project is more of a design piece to show my knowledge of typography and layout. The purpose of this project was to take an image and recreate a movie poster from just the background image.";
+ThirdProject.textContent = myThirdContent;
+
+// created variable for "Project 4" text content 
+var ThirdProject = document.getElementById("FourthProject");
+var myThirdContent = "This last project shows my ability with CSS in order to design for mobile first so the page can effectively be manipulated into different sized formats. As the page dimensions become smaller, the image shrinks to fit the size and a hamburger navigation icon takes over for the original navbar.";
 ThirdProject.textContent = myThirdContent;
 
